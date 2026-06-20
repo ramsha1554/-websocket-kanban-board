5108df9ecd58189104f5786410f4472f092ba8b9
ramsha1554 <work.rm.13.dec@gmail.com>
2026-06-20 15:27:09 +0530

    remove duplicate header from App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import KanbanBoard from "./components/KanbanBoard";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Real-time Kanban Board</h1>
-      <KanbanBoard />
-    </div>
-  );
+export default function App() {
+  return <KanbanBoard />;
 }
-
-export default App;
